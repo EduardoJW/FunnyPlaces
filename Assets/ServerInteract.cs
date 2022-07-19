6904ecaf9da1bfed92ab53cd65f093d292284660
EduardoJW <eduardojw@gmail.com>
2022-07-19 20:02:18 -0300

    finalupdatev1

---
+++ Assets/ServerInteract.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ServerInteract : NetworkBehaviour
+{
+    [Header("Camera")]
+    public GameObject serverCamera;
+    private Camera cam;
+
+    public GameObject clickedObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isServer)
+        {
+            cam = serverCamera.GetComponent<Camera>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isServer) return;
+         
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    clickedObject = hit.collider.gameObject;
+                    if (clickedObject.tag == "Tree")
+                    {
+                    clickedObject.transform.GetComponent<TreeBirdSpawn>().Bird = true;
+                        /*
+                        GameObject[] arrayGO = GameObject.FindGameObjectsWithTag("Player");
+                        foreach (GameObject gameobject in arrayGO)
+                        {
+                            NetworkIdentity nId = gameobject.GetComponent<NetworkIdentity>();
+                            clientbirdspawn(nId.connectionToClient, clickedObject);
+                        }*/
+                    }
+                }
+            }
+        
+
+    }
+
+    /*[TargetRpc]
+    public void clientbirdspawn(NetworkConnection client, GameObject clickedObject) 
+    {
+        clickedObject.transform.Find("Bird").gameObject.SetActive(true);
+    }*/
+}
---
+++ Assets/TreeBirdSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class TreeBirdSpawn : NetworkBehaviour
+{
+    [SyncVar]
+    public bool Bird = false;
+    public bool BirdalreadyTrue = false;
+    public GameObject self;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Bird) 
+        {
+            if (!BirdalreadyTrue) 
+            { 
+                gameObject.transform.Find("Bird").gameObject.SetActive(true);
+                GameObject[] arrayGO = GameObject.FindGameObjectsWithTag("Player");
+                foreach (GameObject gameobject in arrayGO)
+                {
+                    NetworkIdentity nId = gameobject.GetComponent<NetworkIdentity>();
+                    clientbirdspawn(nId.connectionToClient, self);
+                }
+                BirdalreadyTrue = true;
+            }
+        }
+    }
+    [TargetRpc]
+    public void clientbirdspawn(NetworkConnection client, GameObject self)
+    {
+        self.transform.Find("Bird").gameObject.SetActive(true);
+    }
+}
